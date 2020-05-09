097eb00caa6a2adcea3c94081021a8a39e13b722
nilon <nilon@debian.ramoneando>
2020-05-09 20:58:05 -0300

    minor css and js

---
+++ abc/main.js
@@ -0,0 +1,8 @@
+// alert('hola mundo!');
+var nombre = "Benjamín Juárez";
+var altura = 165;
+var concatenacion = nombre + " " + altura;
+// document.write(nombre + " " + altura);
+// document.write(concatenacion);
+var datos = document.getElementById("datos");
+datos.innerHTML = concatenacion;
